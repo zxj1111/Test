d4af1ef22c7001b08ff22531c953b68dae7b8909
zxj1111 <857950259@qq.com>
2018-07-16 09:26:48 +0800

    first commit

---
+++ Test00/Test00/Test00.cpp
@@ -0,0 +1,17 @@
+// Test00.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "Test00.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	Test00 te;
+	cout << "1+2=" << te.add(1, 2) << endl;
+	cout << "2-1=" << te.sub(2, 1) << endl;
+    return 0;
+}
+
---
+++ Test00/Test00/Test00.h
@@ -0,0 +1,13 @@
+#pragma once
+class Test00
+{
+public:
+	int add(int n1,int n2)
+	{
+		return n1 + n2;
+	}
+	int sub(int n1, int n2)
+	{
+		return n1 - n2;
+	}
+};
